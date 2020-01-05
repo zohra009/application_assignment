271f3387c24c43b5c5c1addc1a663c771dc62906
zor009 <anzohra@outlook.com>
2020-01-05 18:18:55 -0500

    random quote generator

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-
-app.get('/quote', (req, res) => {
+let quotes = require('random-movie-quotes')
+app.get('/quote', (req, res) => { 
+  console.log('the response will be sent by the next function ...')
   res.json({
     quote: quotes.getQuote()
   });
